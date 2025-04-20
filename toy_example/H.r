9ccd36562f6f6f631164add129c0bb4f1cb1693b
Nandini Bhattad <141108583+NandiniBhattad13@users.noreply.github.com>
2025-04-20 21:52:45 +0530

    Update H.r

--- toy_example/H.r
+++ toy_example/H.r
@@ -1,12 +1,15 @@
-H = function(n,x) {
+H = function(n,x) 
+{
     value = 0
     if (n == 0) value = 1
     if (n == 1) value = x
-    if (n >= 2) {
+    if (n >= 2) 
+    {
         HH = rep(0,n+1)
         HH[1] = 1
         HH[2] = x
-        for (nn in c(2:n)) {
+        for (nn in c(2:n)) 
+        {
             HH[nn + 1] = x * HH[nn] - (nn - 1) * HH[nn-1]
         }
 
