fb545c8dc4038c72f492d0474229b80f66e3df32
Nandini Bhattad <141108583+NandiniBhattad13@users.noreply.github.com>
2025-04-20 21:53:26 +0530

    Update classify.r

--- toy_example/classify.r
+++ toy_example/classify.r
@@ -1,7 +1,9 @@
-classify = function(type,xObs,xGenerated,classifyParam=NULL,method="logistic",nfolds=5,shuffle=FALSE) {
+classify = function(type,xObs,xGenerated,classifyParam=NULL,method="logistic",nfolds=5,shuffle=FALSE) 
+{
     n = length(xObs)
     
-    if(shuffle) {
+    if(shuffle) 
+    {
         xObs = xObs[sample(n)]
         xGenerated = xGenerated[sample(n)]
     }
@@ -9,32 +11,42 @@ classify = function(type,xObs,xGenerated,classifyParam=NULL,method="logistic",nf
     predictions = rep(NA,n)
 
     idx.test.start = round(seq(1,n+1,length.out=nfolds+1))
-    for(i in 1:nfolds) {
+    for(i in 1:nfolds) 
+    {
         idx.test = idx.test.start[i]:(idx.test.start[i+1]-1)
         n.test = length(idx.test)
         idx.train = (1:n)[-idx.test]
         n.train = length(idx.train)
         x.train = c( xObs[idx.train], xGenerated[idx.train] )
         y.train = c( rep(0,n.train), rep(1,n.train) )
-        if(type == 0) {
+        if(type == 0) 
+        {
             x.test = xObs[idx.test]
             y.test = rep(0,n.test)
-        } else {
+        } 
+        else 
+        {
             x.test = xGenerated[idx.test]
             y.test = rep(1,n.test)
         }
-        if(method == "logistic") {
+        if(method == "logistic") 
+        {
             data.train = data.frame(x = x.train, x2 = x.train^2)
             fit = glm(y.train ~ ., family = binomial("logit"), data = data.train)
             data.test = data.frame(x = x.test, x2 = x.test^2)
             predictions[idx.test] = predict(object = fit, type = "response", newdata = data.test)
-        } else if(method == "knn") {
+        } 
+        else if(method == "knn") 
+        {
             kk = classifyParam$kk
-            if(type == 0) {
+            if(type == 0) 
+            {
                 res = knn(train = matrix(x.train,ncol=1), test = matrix(x.test,ncol=1), cl = y.train, k = kk, prob=TRUE)
                 prob = (res == 0) * (1 - attr(res,"prob")) + (res == 1) * attr(res,"prob")
                 predictions[idx.test] = 1.0 - prob
-            } else {
+            } 
+            else 
+            {
                 res = knn(train = matrix(x.train,ncol=1), test = matrix(x.test,ncol=1), cl = y.train, k = kk, prob=TRUE)
                 prob = (res == 0) * (1 - attr(res,"prob")) + (res == 1) * attr(res,"prob")
                 predictions[idx.test] = prob 
