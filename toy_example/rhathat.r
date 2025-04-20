3d519883af9af47638e70e97652320434d3f9df4
Nandini Bhattad <141108583+NandiniBhattad13@users.noreply.github.com>
2025-04-20 21:52:17 +0530

    Update rhathat.r

--- toy_example/rhathat.r
+++ toy_example/rhathat.r
@@ -1,7 +1,9 @@
-rhathat = function(type,yObs,classifyParam,q,sd,xObs,mu,sigma) {
+rhathat = function(type,yObs,classifyParam,q,sd,xObs,mu,sigma) 
+{
   nA = 10
   a = rep(0,nA)
-  for (i in c(1:nA)) {
+  for (i in c(1:nA)) 
+  {
     xGenerated = rnorm(length(xObs),mean = mu,sd = sigma)
     a[i] = classify(type,xObs,xGenerated,classifyParam)
   }
@@ -10,15 +12,19 @@ rhathat = function(type,yObs,classifyParam,q,sd,xObs,mu,sigma) {
   tau = rgeom(1,q) + 10
   
   r = 1
-  if (tau >= 1) {
-    for (n in c(1:tau)) {
+  if (tau >= 1) 
+  {
+    for (n in c(1:tau)) 
+    {
       term = (-1)^n / factorial(n)
       if (n > 10) term = term / ((1.0 - q)^(n-10))
       term = term * H(n,(yObs - a) / sd)
       
-      for (i in c(1:n)) {
+      for (i in c(1:n)) 
+      {
         y0 = 0
-        for (k in c(1:1)) {
+        for (k in c(1:1)) 
+        {
           xGenerated = rnorm(length(xObs),mean = mu,sd = sigma)
           y0 = y0 + classify(type,xObs,xGenerated,classifyParam)
         }
